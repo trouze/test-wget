5948abaf53a2ecc48f9366c278a93f37eb5c8f44
Tyler Rouze <tyler@tylerrouze.com>
2023-02-09 11:37:52 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
+export py=$(which python3 -1)
 echo "Updating Ubuntu and installing python's pip and virtualenv..."
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get install python3-pip && sudo apt-get install python3-venv
 echo "Creating dbt-projects directory and python virtualenv..."
-mkdir ~/dbt-projects && cd ~/dbt-projects && python3 -m venv env
+mkdir ~/dbt-projects && cd ~/dbt-projects && $py -m venv env
 echo "Entering virtualenv and installing dbt dependencies..."
-source env/bin/activate && pip install dbt-core dbt-snowflake dbt-databricks dbt-bigquery dbt-redshift
+source ~/dbt-projects/env/bin/activate
+$py -m pip install dbt-core dbt-snowflake
 echo "Installing VScode extensions and opening VScode"
 code --install-extension innoverio.vscode-dbt-power-user
 code .
