c2fa72d37282483d8abf96848be741b444250335
Tyler Rouze <tyler@tylerrouze.com>
2023-02-09 10:03:55 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get install python3-pip && sudo apt-get install python3-venv
-mkdir ~/dbt-projects && cd ~/dbt-projects
-python3 -m venv env && source env/bin/activate && pip install dbt-core dbt-snowflake dbt-databricks dbt-bigquery dbt-redshift
+mkdir ~/dbt-projects && cd ~/dbt-projects && python3 -m venv env && source env/bin/activate && pip install dbt-core dbt-snowflake dbt-databricks dbt-bigquery dbt-redshift
 code --install-extension innoverio.vscode-dbt-power-user
 code .
